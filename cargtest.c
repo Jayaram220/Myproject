a3bd4b7e548b289aa28daf478ea944b5b8b26c2a
Jayaram <jayaram220@gmail.com>
2020-04-15 22:42:45 +0530

    First Commit

---
+++ cargtest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int Findsqre(int sq)
+{
+	return sq*sq;
+}
+
+int main(int n,char *argv[])
+{
+	for(int i=0; i < n; i++)
+	{
+		int sq = atoi(argv[i]);
+		printf("%d\n",Findsqre(sq));
+	}
+
+}
